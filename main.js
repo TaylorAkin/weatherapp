637d1d43e09184185ea79005a3a21ac332d79d1e
TaylorAkin <taylorakin.ky@gmail.com>
2019-09-24 22:35:15 -0400

    added to first 3 cards

--- main.js
+++ main.js
@@ -3,19 +3,25 @@ document.getElementById('getweatherbtn').addEventListener('click', weather);
 
 
 async function weather() {
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=9e8ed380997e80befb16e74fdb844202&zip=40511')
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=9e8ed380997e80befb16e74fdb844202&zip=42757')
     const myJson = await response.json();
     console.log(JSON.stringify(myJson));
 
-    
-    let city = myJson.name;
-    document.getElementById('city');
-    var citytxt = document.createTextNode(city);
 
-    citytxt.appendChild(city);
+    let cityname = myJson.name;
+    console.log(cityname);
+    document.getElementById("city").innerHTML = cityname;
 
+    let currentWeather = myJson.weather['0'].description;
+    console.log(currentWeather);
+    document.getElementById("currentweather").innerHTML = currentWeather;
 
+    let temperature = myJson.main.temp;
+    document.getElementById("temperature").innerHTML = temperature;
+    console.log(temperature);
 
-    
+    let  = myJson.main.temp;
+    document.getElementById("temperature").innerHTML = temperature;
+    console.log(temperature);
 
 }

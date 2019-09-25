494ba228c41424dfbae4f88ee949362bf9474f05
TaylorAkin <taylorakin.ky@gmail.com>
2019-09-25 11:54:50 -0400

    working project

--- main.js
+++ main.js
@@ -2,26 +2,56 @@ document.getElementById('getweatherbtn').addEventListener('click', weather);
 
 
 
+
+
+
 async function weather() {
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=9e8ed380997e80befb16e74fdb844202&zip=42757')
+
+
+    let zip = document.getElementById('zipcode').value;
+
+
+
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=9e8ed380997e80befb16e74fdb844202&zip=${zip}`);
     const myJson = await response.json();
     console.log(JSON.stringify(myJson));
 
 
-    let cityname = myJson.name;
-    console.log(cityname);
-    document.getElementById("city").innerHTML = cityname;
+    if (myJson.cod == "400") {
 
-    let currentWeather = myJson.weather['0'].description;
-    console.log(currentWeather);
-    document.getElementById("currentweather").innerHTML = currentWeather;
+        alert('non valid zip code')
+    }
 
-    let temperature = myJson.main.temp;
-    document.getElementById("temperature").innerHTML = temperature;
-    console.log(temperature);
+    else {
 
-    let  = myJson.main.temp;
-    document.getElementById("temperature").innerHTML = temperature;
-    console.log(temperature);
+        let cityname = myJson.name;
+        // console.log(cityname);
+        document.getElementById("city").innerHTML = cityname;
+
+        let currentWeather = myJson.weather['0'].description;
+        // console.log(currentWeather);
+        document.getElementById("currentweather").innerHTML = currentWeather;
+
+        let temperature = myJson.main.temp;
+        var tempf = document.getElementById("temperaturef");
+        var tempc = document.getElementById("temperaturec");
+        var tempk = document.getElementById("temperaturek");
+
+
+        tempf.innerHTML = Math.floor((temperature - 273.15) * 9 / 5 + 32) + " " + "F";
+        tempc.innerHTML = Math.floor(temperature - 273.15) + " " + "C";
+        tempk.innerHTML = Math.floor(temperature) + " " + "K";
+
+
+        // console.log(temperature);
+
+        let iconcode = myJson.weather['0'].icon;
+        // console.log(weathericon);
+        var icon = document.getElementById("weathericon");
+        icon.src = `http://openweathermap.org/img/w/${iconcode}.png`;
+        console.log('ta');
+
+
+    }
 
 }
